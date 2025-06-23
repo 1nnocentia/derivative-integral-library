8fd3549cd37512ad040b406e7b6a92a1fa1e8fb8
Idkwhat77 <swissarchipelago@gmail.com>
2025-06-23 16:27:00 +0800

    add central diff function and calclate f(x)

--- main.py
+++ main.py
@@ -1,31 +1,61 @@
 import math
+from central_difference import central_difference_basic
 from forward_difference import forward_difference_first_order
 
-def menu():
-    print("-- Metode Numerikal Turunan dan Integral --")
-    print("Pilih Metode:")
-    print("1. Turunan")
-    print("2. Integral")
-    choice = input("Masukkan pilihan (1/2): ")
-    match choice:
-        case "1": 
-            print("Metode Turunan Apa yang ingin Anda gunakan?")
-            print("1. Forward Difference")
-            print("2. Central Difference")
-            print("3. Backward Difference")
-            sub_choice = input("Masukkan pilihan (1/2/3): ")
-            match sub_choice:
-                case "1":
-                    print("Kamu memilih forward difference.")
-                    print("1. First Order")
-                    print("2. Second Order")
-                    imp_choice = print("Yang mana ingin kamu gunakan(1/2): ")
-                case "2":
-                    print("Central Difference belum diimplementasikan.")
-                case "3":
-                    print("Backward Difference belum diimplementasikan.")
-                case _:
-                    print("Pilihan tidak valid.")
+from function import function_input
 
-if __main__ == "__main__":
+def menu():
+    while True:
+        print("""
+===== Metode Numerikal Turunan dan Integral =====
+1. Turunan
+2. Integral
+3. Exit
+=================================================""")
+        choice = input("Mau pilih metode yang mana? (1/2/3): ")
+        if choice not in ["1", "2", "3"]:
+            print("Pilihan tidak valid. Silakan coba lagi.")
+        match choice:
+            case "1": 
+                print("Metode Turunan Apa yang ingin Anda gunakan?")
+                print("1. Forward Difference")
+                print("2. Central Difference")
+                print("3. Backward Difference")
+                sub_choice = input("Masukkan pilihan (1/2/3): ")
+                match sub_choice:
+                    case "1":
+                        print("Kamu memilih forward difference.")
+                        print("1. First Order")
+                        print("2. Second Order")
+                        imp_choice = print("Yang mana ingin kamu gunakan(1/2): ")
+                    case "2":
+                        print("Kamu memilih central difference.")
+                        function, x, h = function_input()
+
+                        def f(x):
+                            return eval(function, {"x": x, "math": math})
+
+                        print("Hasil: ", central_difference_basic(f, x, h))
+
+                    case "3":
+                        print("Backward Difference belum diimplementasikan.")
+                    case _:
+                        print("Pilihan tidak valid.")
+            case "2":
+                print("Metode Integral Apa yang ingin Anda gunakan?")
+                print("1. Trapezoidal Rule")
+                print("2. Simpson's Rule")
+                sub_choice = input("Masukkan pilihan (1/2): ")
+                match sub_choice:
+                    case "1":
+                        print("Trapezoidal Rule belum diimplementasikan.")
+                    case "2":
+                        print("Simpson's Rule belum diimplementasikan.")
+                    case _:
+                        print("Pilihan tidak valid.")
+            case "3":
+                print("Terima kasih telah menggunakan program ini. Sampai jumpa!")
+                break
+
+if __name__ == "__main__":
     menu()
